ce4242a720c6cb4dbd8e466f71dbe34c6fb3f844
jackholsgrove <jackholsgrove@gmail.com>
2021-02-17 22:01:03 +0000

    added route logic, configured mongodb and mongoose

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const express = require('express');
 // import dotenv for configuration of environmental variables
 require('dotenv').config();
 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
+
+
 // initialize express app
 const app = express();
 // initialize the port by either using the environmental PORT  variable or 5000 if unset
 const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true})
+	.then( () => {
+		return console.log(`Database connected successfully`);
+	})
+	.catch(err => console.log(err));
+
+mongoose.Promise = global.Promise;
+
 
 // configure CORS to allow scripts from other domains  
 app.use((req, res, next) => {
@@ -16,9 +31,15 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use((req, res, next) =>{
-	res.send('Welcome to Express');
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	next()
 });
+
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
