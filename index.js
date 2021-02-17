91effd47529be7be9ffde831520df968936442bf
jackholsgrove <jackholsgrove@gmail.com>
2021-02-17 19:52:06 +0000

    added skeleton routes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// import express
 const express = require('express');
+// import dotenv for configuration of environmental variables
 require('dotenv').config();
 
+// initialize express app
 const app = express();
-
+// initialize the port by either using the environmental PORT  variable or 5000 if unset
 const port = process.env.PORT || 5000;
 
-app.use((reg, res, next) => {
+// configure CORS to allow scripts from other domains  
+app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
